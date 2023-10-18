9fa4cc82246e8ac2d01f62f21fcd0fcf3ce40b8a
emsi06001 <emsi06001@E-PW0234N3>
2023-10-18 13:12:43 +0200

    Add project files.

---
+++ inluppg4.6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace inluppg4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv meddelande");
+            string input = Console.ReadLine();
+            Console.WriteLine("Skriv antal rader förskjutning");
+            int rader = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                for (int j = 0; j <= rader; j++)
+                {
+                    Console.Write(' ');
+                }
+                Console.Write(input[i] + "\n");
+            }
+        }
+    }
+}
